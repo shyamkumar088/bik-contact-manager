28032895f3bbd0951bdd2f9ebd96d66d16707cf5
nawarajpokharel@gmail.com <nawarajpokharel@gmail.com@d7934f99-6b6a-dcfc-a53e-28cf00dd23af>
2011-05-25 12:39:49 +0000

    added hibernate stuff

--- trunk/src/com/sample/contact/client/contactServiceAsync.java
+++ trunk/src/com/sample/contact/client/contactServiceAsync.java
@@ -1,8 +1,13 @@
 package com.sample.contact.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.sample.contact.shared.domain.Person;
+
+import java.util.*;
 
 public interface contactServiceAsync {
 
   void getMessage(String msg, AsyncCallback<String> async);
+
+  void findAllPersons(AsyncCallback<List<Person>> async);
 }
--- trunk/src/com/sample/contact/server/contactServiceImpl.java
+++ trunk/src/com/sample/contact/server/contactServiceImpl.java
@@ -2,11 +2,24 @@ package com.sample.contact.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.sample.contact.client.contactService;
+import com.sample.contact.server.dao.BasicDAO;
+import com.sample.contact.shared.domain.Person;
+
+import java.util.*;
 
 public class contactServiceImpl extends RemoteServiceServlet implements contactService {
+  private BasicDAO basicDAO;
+
+  public void setBasicDAO(BasicDAO basicDAO) {
+    this.basicDAO = basicDAO;
+  }
 
   // Implementation of sample interface method
   public String getMessage(String msg) {
     return "Client said: \"" + msg + "\"<br>Server answered: \"Hello!\"";
+  }
+
+  public List<Person> findAllPersons() {
+    return basicDAO.findAllPerson();
   }
 }
